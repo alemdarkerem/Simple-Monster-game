ba09976d4158dff879e50b861e24328ee6e2c360
Kerem Alemdar <alemdarkerem1907@gmail.com>
2023-11-09 13:06:41 +0300

    Added 'Game Over' Section.

---
+++ app.js
@@ -0,0 +1,77 @@
+function getRandomValue(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+const app = Vue.createApp({
+    data() {
+        return {
+            monsterHealth: 100,
+            playerHealth: 100,
+            currentRound: 0,
+            winner: null,
+
+        };
+
+    },
+    watch: {
+        playerHealth(value) {
+            if(value <= 0 && this.monsterHealth <= 0){
+                // DRAW
+                this.winner = 'draw';
+            }else if (value <= 0) {
+                //Player Lost
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value) {
+            if(value <= 0 && this.playerHealth <= 0){
+                // DRAW
+                this.winner = 'draw';
+            } else if (value <= 0){
+                //Monster Lost
+                this.winner = 'player';
+            }
+        }
+    },
+    computed: {
+        monsterBarStyles() {
+            return {width: this.monsterHealth + '%'}
+        },
+        playerBarStyles() {
+            return {width: this.playerHealth + '%'}
+        },
+        mayUseSpecialAttack() {
+            return  this.currentRound === 0 || this.currentRound % 3 !== 0;
+        }
+    },
+    methods: {
+        attackMonster() {
+            this.currentRound++;
+            const attackValue = getRandomValue(5, 12);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
+        },
+        specialAttackMonster(){
+            this.currentRound++;
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        healPlayer() {
+            this.currentRound++;
+            const healValue = getRandomValue(8, 20);
+            if (this.playerHealth + healValue > 100) {
+                this.playerHealth = 100;
+            }else {
+                this.playerHealth += healValue;
+            }
+            this.attackPlayer();
+        }
+    }
+
+});
+app.mount('#game')
